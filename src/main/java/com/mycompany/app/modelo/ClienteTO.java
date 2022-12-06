876d44c96e6588894fa0563bc784a83ad1607c70
jhljoel <joel.huillca77@gmail.com>
2022-12-05 21:42:39 -0500

    para la primera nota

---
+++ src/main/java/com/mycompany/app/modelo/ClienteTO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.app.modelo;
+
+import lombok.Data;
+
+/**
+ *
+ * @author PC
+ */
+@Data
+public class ClienteTO {
+    public String dniruc, nombresrs, tipo;
+
+}
